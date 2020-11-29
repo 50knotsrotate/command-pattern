88a48b98a4ca898443ff647b7ea0e80fbc40a107
= <=>
2020-11-29 16:43:19 -0500

    semicolons

--- Lightbulb.js
+++ Lightbulb.js
@@ -7,9 +7,11 @@ var LightBulb = /** @class */ (function () {
     LightBulb.prototype.on = function () {
         console.log('Lightbulb is on!');
     };
+    ;
     LightBulb.prototype.off = function () {
         console.log('lightbulb is off!');
     };
+    ;
     return LightBulb;
 }());
 exports.LightBulb = LightBulb;
--- Lightbulb.ts
+++ Lightbulb.ts
@@ -1,9 +1,9 @@
 export class LightBulb { 
     on() { 
         console.log('Lightbulb is on!')
-    }
+    };
 
     off() { 
         console.log('lightbulb is off!');
-    }
+    };
 };
